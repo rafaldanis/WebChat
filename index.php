7db86ad727eba69637315398cfa8ef05819945f0
Rafał Daniś <r.danis88@gmail.com>
2019-02-05 22:54:49 +0100

    lay

--- index.php
+++ index.php
@@ -13,11 +13,14 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300" rel="stylesheet"> 
     <link rel="stylesheet" href="/css/style.css" />
     <script>
         $( document ).ready(function() {
             $("form[data-submit='message']").submit(function(){
                 var message = $("*[data-type='message']").val();
+                $("*[data-type='message']").val('');
+                $('#scroll').animate({scrollTop:$('#chat').height()}, 'slow');
                 sendMessage(message);
                 return false;
             });
@@ -53,7 +56,8 @@
                         $("[data-content='users']").html($("[data-content='users']").html()+'<li>'+data.all_users[user]+'</li>');
                     }
                 
-                    $("[data-content='chat']").html(content + '<div class="parent_message"><div class="message received-message"><span> '+data.user+ ':' + data.msg + '</span></div></div>');
+                    $("[data-content='chat']").html(content + '<div class="parent_message"><div class="message received-message"><span class="text-grey"> '+data.user+ ':</span> <span>' + data.msg + '</span></div></div>');
+                    $('#scroll').animate({scrollTop:$('#chat').height()}, 'slow');
                 }
             };
             
@@ -66,27 +70,27 @@
         </div>
     
         <div id="page-content-wrapper" class="d-flex align-items-center">
-            <div class="container d-flex justify-content-center">
+            <div id="scroll" class="container d-flex justify-content-center" style="height: 50%; overflow-y: auto;">
                 <div id="content" class="col-12">
                     <div class="" data-content="chat" id="chat"></div>
                     <?php
-                        echo '<div id="form">';
-                            echo '<div class="d-none" data-content="nick">'.USER_NICK.'</div>';
-                            echo '<form id="message" class="form-signin" data-submit="message" data-ret="false" type="POST">';
-                                echo '<p>Twój nick: <span>'.USER_NICK.'</span></p>';
-                                echo '<textarea class="form-control" data-type="message" placeholder="Type your message here..."></textarea>';
-                                echo '<button type="submit" class="btn btn-lg btn-primary btn-block btn-signin">Wyślij</button';
+                        echo '<div id="form" class="fixed-bottom">';
+                            echo '<div class="d-none" data-content="nick">' . USER_NICK . '</div>';
+                            echo '<form id="message" class="form-signin p-3" data-submit="message" data-ret="false" type="POST">';
+                                echo '<p>Hej <span>' . USER_NICK . '</span> poniżej wpisz wiadomość</p>';
+                                echo '<input class="form-control" data-type="message" placeholder="Wpisz wiadomość..." />';
+                               // echo '<button type="submit" class="btn btn-lg btn-primary btn-block btn-signin">Wyślij</button';
                             echo '</form>';
                         echo '</div>';
                     ?>
                 </div>
             </div>
         </div>
-        <div class="d-flex align-items-center">
+        <!--<div class="d-flex align-items-center">
             <div class="container d-flex justify-content-center">
                 <a href="#users" class="btn btn-secondary" data-event="users">Zobacz wszystkich użytkowników czatu</a>
             </div>
-        </div>
+        </div>-->
     </div>    
 </body>
 </html>
